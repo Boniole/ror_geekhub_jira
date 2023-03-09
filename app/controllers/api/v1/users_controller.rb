f309300c219cc96f377b865e1396c2e02d79a407
Nazar Volynets <nazikvol55@gmail.com>
2023-03-09 14:42:33 +0200

    Added Users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :created
+    else
+      render json: { error: 'Invalid name or password' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :password)
+  end
+end
