a3238ee3ca9567a92c60c51771abf6ed5926727e
Nazar Volynets <nazikvol55@gmail.com>
2023-03-09 14:58:27 +0200

    Modified routes and fixed params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :password)
+    params.require(:user).permit(:name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "desc#index"
+  root 'desc#index'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+    end
+  end
 end
