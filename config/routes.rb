dd47b81f843a997494687bbe28a25c082769b99c
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-03-10 21:34:29 +0200

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       post '/login', to: 'users#login'
+      resources :projects
+      resources :desks
       resources :tasks do 
         member do
           get :comments
