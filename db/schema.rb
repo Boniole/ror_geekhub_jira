fa617637220010025dffe82a662028c09bb676fd
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-03-22 18:05:08 +0200

    Add admin column for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_143123) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_155219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_143123) do
     t.string "email"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "admin", default: false, null: false
   end
 
   add_foreign_key "columns", "desks"
--- test/fixtures/users.yml
+++ test/fixtures/users.yml
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  admin                  :boolean          default(FALSE), not null
 #  email                  :string
 #  last_name              :string
 #  name                   :string
