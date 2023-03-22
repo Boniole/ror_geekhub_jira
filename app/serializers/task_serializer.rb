57f7d7b80fd7f7a31efca2441553fa7f5ed38d0a
Oleksii Mikhno <xioflesh@gmail.com>
2023-03-22 20:30:57 +0200

    add assignee

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -35,7 +35,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class TaskSerializer < ActiveModel::Serializer
-  attributes :user, :column, :title, :description, :estimate, :label, :priority, :type_of, :status, :start, :end
+  attributes :user, :assignee, :column, :title, :description, :estimate, :label, :priority, :type_of, :status, :start, :end
 
   has_one :user, serializer: UserSerializer
   has_one :column, serializer: ColumnSerializer
