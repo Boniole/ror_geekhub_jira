dddffe4eb1711eea4095c366773ad972bdff4eb8
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-03-24 23:43:21 +0200

    Add task policy

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,29 +1,39 @@
 class TaskPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
+  attr_reader :user, :task
 
-  def create?
-    record.project.users.include?(user)
+  def initialize(user, task)
+    @user = user
+    @task = task
   end
 
   def show?
-    record.project.users.include?(user)
+    project_member?
+  end
+
+  def create?
+    project_member?
   end
 
   def update?
-    admin_or_owner? || record.executor == user
+    admin_or_owner_or_assignee?
   end
 
   def destroy?
-    admin_or_owner? || record.executor == user
+    admin_or_owner?
   end
 
   private
 
-  def admin_or_owner?
-    record.project.admin == user || record.project.owner == user
+  def project_member?
+    Membership.where(project_id: task.project_id, user_id: user.id).exists?
   end
+
+  def admin_or_owner?
+    user.admin? || task.user_id == user.id
+  end
+
+  def admin_or_owner_or_assignee?
+    user.admin? || task.assignee_id == user.id || task.user_id == user.id
+  end
+
 end
