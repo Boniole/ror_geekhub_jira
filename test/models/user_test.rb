ca4f4e652fa23101f78b3bcb5dd94727d4a19dc0
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-03-24 23:40:30 +0200

    Add model relation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,6 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
-#  admin                  :boolean          default(FALSE), not null
 #  email                  :string
 #  last_name              :string
 #  name                   :string
