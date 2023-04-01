eff51124724f8ed0c25539cad16de3c222c7af96
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-04-01 23:05:47 +0300

    refactoring project and desk controllers

--- app/controllers/api/v1/desks_controller.rb
+++ app/controllers/api/v1/desks_controller.rb
@@ -28,8 +28,8 @@ class Api::V1::DesksController < ApplicationController
 
   def destroy
     @desk.destroy
-  rescue ActiveRecord::InvalidForeignKey
-    render json: { errors: 'You cannot delete a board while you have dependencies' }, status: :unauthorized
+  rescue ActiveRecord::InvalidForeignKey => e
+    render json: { error: e.message }, status: :unauthorized
   end
 
   private
@@ -39,10 +39,10 @@ class Api::V1::DesksController < ApplicationController
     if @desk.user_id == @current_user.id
       @desk
     else
-      render json: { errors: 'User is not authorized to access this resource' }, status: :unauthorized
+      render json: { error: 'User is not authorized to access this resource' }, status: :unauthorized
     end
-  rescue ActiveRecord::RecordNotFound
-    render errors.full_messages, status: :not_found
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
   end
 
   def set_desks
@@ -52,7 +52,7 @@ class Api::V1::DesksController < ApplicationController
 
   def desk_params
     params.require(:desk).permit(:name, :project_id)
-  rescue ActionController::ParameterMissing
-    render json: { error: 'Missing required parameter(s)' }, status: :bad_request
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
   end
 end
