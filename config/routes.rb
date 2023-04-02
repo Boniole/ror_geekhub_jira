bd59fd106883d14526ab980d3b7fb578c426164e
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-04-02 23:12:01 +0300

    fix routes and config storage.yml

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/about_user', to: 'users#about_current_user'
       resources :users do
-        resources :documents
+        resources :documents, except: :update
         member do
           get :comments
         end
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       post '/forgot', to: 'passwords#forgot'
       post '/reset', to: 'passwords#reset'
       resources :projects do
-        resources :documents
+        resources :documents, except: :update
       end
       resources :desks do
         member do
@@ -23,14 +23,14 @@ Rails.application.routes.draw do
       end
       resources :columns
       resources :tasks do
-        resources :documents
+        resources :documents, except: :update
         member do
           get :comments
         end
       end
 
       resources :comments
-      resources :documents
+      resources :documents, except: :update
       # get '/github/show', to: 'github#show'
       resources :githubs do
         post 'show', on: :collection
