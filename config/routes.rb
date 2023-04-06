b38e5d54a91a59a6759aa06c0f383ad35f3cc93c
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-06 21:54:21 +0300

    fix tasks

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
           end
         end
       end
-
+      resources :tasks
       resources :comments
       resources :documents
       # get '/github/show', to: 'github#show'
