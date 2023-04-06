3c76cc8b87473fd6b4fe8f8e7fa1df55cd54ed85
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-04-06 15:57:55 +0300

    fix migration for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_103104) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_191823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "columns", force: :cascade do |t|
     t.text "name"
-    t.string "columnable_type", null: false
-    t.bigint "columnable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["columnable_type", "columnable_id"], name: "index_columns_on_columnable"
+    t.integer "ordinal_number", default: 0
+    t.bigint "desk_id", null: false
+    t.index ["desk_id"], name: "index_columns_on_desk_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -74,11 +46,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_103104) do
   end
 
   create_table "documents", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "task_id"
+    t.bigint "comment_id"
+    t.string "name", null: false
+    t.string "document_type", null: false
+    t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "documentable_type", null: false
-    t.bigint "documentable_id", null: false
-    t.index ["documentable_type", "documentable_id"], name: "index_documents_on_documentable"
+    t.index ["comment_id"], name: "index_documents_on_comment_id"
+    t.index ["project_id"], name: "index_documents_on_project_id"
+    t.index ["task_id"], name: "index_documents_on_task_id"
+    t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
   create_table "memberships", force: :cascade do |t|
@@ -136,11 +116,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_103104) do
     t.string "github_token"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "columns", "desks"
   add_foreign_key "comments", "tasks"
   add_foreign_key "comments", "users"
   add_foreign_key "desks", "projects"
+  add_foreign_key "documents", "comments"
+  add_foreign_key "documents", "projects"
+  add_foreign_key "documents", "tasks"
+  add_foreign_key "documents", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "tasks", "desks"
   add_foreign_key "tasks", "projects"
