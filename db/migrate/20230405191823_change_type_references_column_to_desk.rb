4c6acf45f0bea80e7de44089cfd439060e167c8c
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-04-06 16:07:51 +0300

    fix migration for deploy/change earlier migrate

--- db/migrate/20230405191823_change_type_references_column_to_desk.rb
+++ db/migrate/20230405191823_change_type_references_column_to_desk.rb
@@ -1,11 +1,9 @@
 class ChangeTypeReferencesColumnToDesk < ActiveRecord::Migration[7.0]
   def up
-    remove_reference :columns, :columnable, polymorphic: true, index: true
     add_reference :columns, :desk, foreign_key: true, null: false
   end
 
   def down
-    remove_reference :columns, :desk, foreign_key: true
     add_reference :columns, :columnable, polymorphic: true, index: true, null: false
   end
 end
