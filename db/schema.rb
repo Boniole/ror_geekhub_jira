de30d2d7a73e6b9c375b177f93606b4a53cc97f1
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-09 22:11:12 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_09_174319) do
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_145257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,14 +103,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_145257) do
     t.integer "tasks_count", default: 0, null: false
     t.string "git_url"
     t.string "git_name"
-    t.index ["user_id"], name: "index_projects_on_user_id"
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.integer "status", default: 0
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "tasks_count", default: 0, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
