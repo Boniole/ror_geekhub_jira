217a742b16c616dcc96f5ed096b1af783914b331
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-09 17:45:42 +0300

    add git_url for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@
 # Table name: projects
 #
 #  id          :bigint           not null, primary key
+#  git_url     :string
 #  name        :string
 #  status      :integer          default("open")
 #  tasks_count :integer          default(0), not null
---
+++ db/migrate/20230409144034_add_field_git_url_to_progects.rb
@@ -0,0 +1,5 @@
+class AddFieldGitUrlToProgects < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projects, :git_url, :string
+  end
+end
