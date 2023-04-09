3aabae7b3a280972e3b0c6a7b81cc6453f304f95
Serhii Dratovanyi <boniole@outlook.com>
2023-04-09 22:39:38 +0300

    Add global policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,5 +1,11 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
-  policy.img_src :self, :https
-  policy.connect_src :self, :https, 'https://kotello.space'
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https
+
+  # Specify URI for violation reports
+  policy.report_uri "/csp-violation-report-endpoint"
 end
