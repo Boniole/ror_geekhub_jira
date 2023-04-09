3ae90efd9456a34499738e16a9e727c391f3c59b
Serhii Dratovanyi <boniole@outlook.com>
2023-04-09 22:30:14 +0300

    Fix policy add policy.connect_src

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,4 +1,5 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.img_src :self, :https
+  policy.connect_src :self, :https, 'https://kotello.space'
 end
