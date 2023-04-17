bb11afd8870132c8882ab30eca82dfe6eb483d79
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-17 14:15:52 +0300

    change save to update @project

--- app/models/github_repository.rb
+++ app/models/github_repository.rb
@@ -6,7 +6,7 @@ class GithubRepository
 
   validates :project_id, numericality: { only_integer: true }
   validates :name, length: { in: 3..30 }
-  validates :description, presence: true, length: { in: 3..2500 }, allow_blank: true
+  validates :description, length: { in: 3..2500 }, allow_blank: true
   validates :private, inclusion: [true, false]
   validates :has_issues, inclusion: [true, false]
   validates :has_downloads, inclusion: [true, false]
