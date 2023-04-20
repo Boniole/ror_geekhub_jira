752c3d5fe16eba35f06a568efa1f95b83e70f273
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-20 23:54:53 +0300

    upda te Schema Information

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id         :bigint           not null, primary key
+#  role       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  project_id :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_memberships_on_project_id  (project_id)
+#  index_memberships_on_user_id     (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
