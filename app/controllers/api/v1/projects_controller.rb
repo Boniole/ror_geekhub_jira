f8e330341d52d979d56424d2c86357fc20937e22
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-04-21 22:56:08 +0300

    Refactor project controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ProjectsController < ApplicationController
-  before_action :project_params, only: %i[create update]
+  before_action :project_params, only: %i[create]
   before_action :set_projects, only: :index
   before_action :set_project, :authorize_user, only: %i[show update destroy add_member delete_member]
-  # before_action memberships, only: %i[show update destroy add_member delete_member]
+  before_action :memberships, only: %i[update destroy add_member delete_member]
 
   def index
     render json: @projects, status: :ok
@@ -13,16 +13,12 @@ class Api::V1::ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_params)
-    @project.user_id = @current_user.id
-
-    # before_auth authorize @project || Project
+    @project = current_user.projects.new(project_params)
     authorize @project
-
     if @project.save
-      # current_user.new
-      membership = @project.memberships.new(user_id: @current_user.id, role: :admin)
+      membership = @project.memberships.new(user_id: current_user.id, role: :admin)
       membership.save!
+
       render json: @project, status: :created, serializer: Api::V1::ProjectSerializer
     else
       render json: @project.errors, status: :unprocessable_entity
@@ -39,15 +35,12 @@ class Api::V1::ProjectsController < ApplicationController
 
   def add_member
     user = User.find(params[:user_id])
-    # remove .first
-    # rename to memberships
-    existing_membership = @project.memberships.where(user:).first
-    # .any?
-    if existing_membership
+    existing_membership = memberships.where(user:)
+
+    if existing_membership.any?
       render json: { error: 'User is already a member of the project' }, status: :unprocessable_entity
     else
-      # build rename to new
-      membership = @project.memberships.build(user:, role: 'member')
+      membership = memberships.new(user:)
 
       if membership.save
         render json: membership, status: :created, serializer: Api::V1::MembershipSerializer
@@ -58,9 +51,7 @@ class Api::V1::ProjectsController < ApplicationController
   end
 
   def delete_member
-    # move to before_action
-    membership = @project.memberships.find_by(user_id: params[:user_id])
-    # @memberships.find_by(user_id: params[:user_id])
+    membership = memberships.find_by(user_id: params[:user_id])
     # authorize @project #kill action before Project current_user
     membership.destroy
   end
@@ -71,18 +62,24 @@ class Api::V1::ProjectsController < ApplicationController
 
   private
 
+  def memberships
+    @project.memberships
+  end
+
   def authorize_user
     authorize @project || Project
   end
 
+  # TODO: method update and delete cant see current_user
   def set_project
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
     render json: { errors: e.message }, status: :not_found
   end
 
   def set_projects
-    @projects = Project.where(user_id: @current_user)
+    # TODO: think about project where user is member
+    @projects = current_user.projects
   end
 
   def project_params
