520da5078c4ac24dee6e5d4456894c36670f0f34
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-04-22 15:21:09 +0300

    Fix user controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,14 +1,13 @@
 class Api::V1::AuthenticationController < ApplicationController
-  include Regexable
+  include TokenGenerationable
   skip_before_action :authorize_request
 
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token:, expiration_date: time.strftime(DATE_FORMAT),
-                     first_name: @user }, status: :ok
+      token_data = generate_token(@user.id)
+      render json: { token: token_data[:token], expiration_date: token_data[:expiration_date],
+                     user: @user }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
--- app/controllers/concerns/regexable.rb
+++
@@ -1,5 +0,0 @@
-module Regexable
-  extend ActiveSupport::Concern
-
-  DATE_FORMAT = '%m-%d-%Y %H:%M'.freeze
-end
