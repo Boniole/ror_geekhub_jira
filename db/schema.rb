37827d509ac8ff301737218884f13caf2fb8dad0
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-22 20:53:07 +0300

    add column_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_22_083655) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_172034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_22_083655) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "columns_count", default: 0, null: false
     t.index ["project_id"], name: "index_desks_on_project_id"
   end
 
