5a37c4a10a1dbd8655a426d5251748364e6dba88
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-04-26 12:11:00 +0300

    Fix render password contr

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -19,35 +19,33 @@ class Api::V1::PasswordsController < ApplicationController
                                      to: user.email,
                                      token: user.reset_password_token,
                                      username: user.first_name }.to_json)
-      render json: { status: 'ok' }, status: :ok
+      render_success(data: 'ok')
     else
-      render json: { errors: ['Email address not found. Please check and try again.'] }, status: :not_found
+      render_error(errors: ['Email address not found. Please check and try again'], status: :not_found)
     end
   end
 
   def reset_password
-    return render json: { errors: 'Token not present' } if params[:email].blank?
+    render_error(errors: ['Token not present']) if params[:email].blank?
 
     user = User.find_by(reset_password_token: params[:token])
 
     if user.present? && user.password_token_valid?
       if user.reset_password!(params[:password])
-        render json: { status: 'ok' }, status: :ok
+        render_success(data: 'ok')
       else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        render_error(errors: user.errors.full_messages)
       end
     else
-      render json: { errors: ['Link not valid or expired. Try generating a new link.'] }, status: :not_found
+      render_error(errors: ['Link not valid or expired. Try generating a new link'], status: :not_found)
     end
   end
 
   def update_password
-    unless current_user.authenticate(params[:old_password])
-      return render json: { errors: 'Old password is incorrect' }, status: :unprocessable_entity
-    end
+    render_error(errors: ['Old password is incorrect']) unless current_user.authenticate(params[:old_password])
 
     # TODO: need to add password validation
-    if current_user.update(password: params[:password]) # current_user.save(validate: false)
+    if current_user.update!(password: params[:password])
 
       render_success(data: current_user, serializer: Api::V1::UserSerializer)
     else
