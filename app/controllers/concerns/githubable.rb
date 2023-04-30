1448a41af6871bdb149771edc3ce27921463b822
Oleksii Mikhno <xioflesh@gmail.com>
2023-04-30 15:41:43 +0300

    split github concern

--- app/controllers/concerns/githubable.rb
+++ app/controllers/concerns/githubable.rb
@@ -1,63 +1,10 @@
-
 module Githubable
   extend ActiveSupport::Concern
+  include GithubBranchable, GithubRepositoryable
 
   def github_client
     Octokit::Client.new(access_token: current_user.github_token, auto_paginate: true)
   rescue Octokit::BadRequest
     render_error(errors: 'Github token invalid or empty!', status: :not_found)
-  end
-
-  def git_create_repo
-    @repo = github_client.create_repository(
-      params[:name],
-      description: params[:description],
-      private: params[:private],
-      has_issues: params[:has_issues],
-      has_downloads: params[:has_downloads]
-    )
-  rescue Octokit::Error => e
-    render_error(errors: "Repository #{e.errors.first[:message]}")
-  end
-
-  def git_update_repo
-    @repo = github_client.edit_repository(
-      current_project.git_repo,
-      description: params[:description],
-      private: params[:private],
-      has_issues: params[:has_issues],
-      has_downloads: params[:has_downloads]
-    )
-  rescue Octokit::InvalidRepository => e
-    render_error(errors: e.message)
-  end
-
-  def git_find_repo
-    @repo = github_client.repository(current_project.git_repo)
-  rescue Octokit::InvalidRepository => e
-    render_error(errors: e.message)
-  end
-
-  def git_create_branch
-    @new_branch_name = "heads/#{@task.type_of}/#{@task.tag_name}/#{params[:branch_name]}"
-
-    github_client.create_ref(
-      current_project.git_repo,
-      @new_branch_name,
-      params[:sha]
-    )
-  rescue Octokit::UnprocessableEntity => e
-    render_error(errors: e.message)
-  end
-
-  def git_get_brenches
-    branches = github_client.branches(current_project.git_repo)
-    branches.map { |branch| { name: branch.name, sha: branch.commit.sha } }
-  rescue Octokit::UnprocessableEntity => e
-    render_error(errors: e.message)
-  end
-
-  def git_task_brenches(task_name)
-    git_get_brenches.select { |branch| branch[:name].match(%r{/#{Regexp.escape(task_name)}/}) }
   end
 end
