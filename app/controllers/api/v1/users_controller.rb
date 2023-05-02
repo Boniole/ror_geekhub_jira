205b51476f2360007eaf63013f75cd241e8003e8
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-05-02 23:31:08 +0300

    Uncommented nuts

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,13 +18,13 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       token_data = generate_token(@user.id)
-      # Move to concerns
-      # nats_publish('service.mail', { class: 'account',
-      #                                type: 'account_register_new',
-      #                                language: 'en',
-      #                                password: @user.password,
-      #                                to: @user.email,
-      #                                username: @user.first_name }.to_json)
+      Move to concerns
+      nats_publish('service.mail', { class: 'account',
+                                     type: 'account_register_new',
+                                     language: 'en',
+                                     password: @user.password,
+                                     to: @user.email,
+                                     username: @user.first_name }.to_json)
       render_success(data: {
                        token: token_data[:token],
                        expiration_date: token_data[:expiration_date],
