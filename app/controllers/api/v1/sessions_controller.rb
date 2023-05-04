33be82d50c0ca5c460539eeba40e9175ac600058
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-05-04 22:13:10 +0300

    Delete comment in session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::SessionsController < ApplicationController
 
   def omniauth
     user = User.from_omniauth(auth)
-    # move dot env Rails.application.secrets.secret_key_base
     token = JWT.encode({ user_id: user.id }, ENV['SECRET_KEY_BASE'])
 
     render_success(data: token)
