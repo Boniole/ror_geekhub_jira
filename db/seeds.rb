3fa6a21be1d8e1b264b5ce2d67923b39dcde4205
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-05 23:49:08 +0300

    upd seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@
 require 'ffaker'
 require_relative 'constants_file'
 
+User.create(first_name: 'John', last_name: 'Dou',
+            email: 'johndou@gmail.com')
+
 20.times do
   user = User.new(first_name: NAME.sample, last_name: FFaker::Name.last_name,
                   email: FFaker::Internet.free_email)
@@ -11,48 +14,39 @@ require_relative 'constants_file'
   user.save!
 end
 
-10.times do |i|
-  user = User.find(i + 1)
-  project = user.projects.create(name: "project ##{i}")
+user = User.first
+project = user.projects.create(name: 'project #1')
 
-  # add memberships
-  project.memberships.create(user_id: user.id, role: 'admin')
+# add memberships
+project.memberships.create(user_id: user.id, role: 'admin')
 
-  10.times do |e|
-    if i < 10
-      project.memberships.create!(user_id: User.find(i + e + 2).id, role: 'member')
-    else
-      project.memberships.create!(user_id: User.find(i - e + 1).id, role: 'member')
-    end
-  end
+10.times do |e|
+  project.memberships.create!(user_id: User.find(e + 3).id, role: 'member')
+end
 
-  # find Desk
-  desk = Desk.find_by(project_id: project.id)
+# find Desk
+desk = project.desks.first
 
-  # create Tasks
-  25.times do
-    description = [FRONT, QA, BACK].sample
+# create Tasks
+25.times do
+  description = [FRONT, QA, BACK].sample
 
-    column = Column.where(desk_id: desk.id).sample
-    task = column.tasks.create(
-      name: "Task for project number #{project.id}",
-      description: description.sample,
-      label: %w[FRONT QA BACK].sample,
-      estimate: '2w',
-      start_date: Date.today,
-      end_date: Date.today + 1,
-      assignee_id: User.all.sample.id,
-      user_id: project.user_id,
-      project_id: project.id,
-      desk_id: desk.id
-    )
+  column = Column.where(desk_id: desk.id).sample
+  task = column.tasks.create!(
+    name: "Task for project number #{project.id}",
+    description: description.sample,
+    label: %w[FRONT QA BACK].sample,
+    estimate: '2w',
+    start_date: Date.today,
+    end_date: Date.today + 1,
+    assignee_id: User.all.sample.id,
+    user_id: project.user_id,
+    project_id: project.id,
+    desk_id: desk.id
+  )
 
-    # create Comments
-    5.times do
-      task.comments.create(
-        body: COMMENT.sample,
-        user_id: project.memberships.sample.id
-      )
-    end
+  # create Comments
+  5.times do
+    task.comments.create!(body: 'COMMENT.sample', user_id: project.memberships.sample.id)
   end
 end
